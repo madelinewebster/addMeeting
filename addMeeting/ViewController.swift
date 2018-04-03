5a108794570cf682d3ef7182d4fb0582d80899bd
Madeline Webster <madelinewebster@Madelines-MacBook-Pro.local>
2018-04-03 08:13:11 -0700

    Initial Commit

---
+++ addMeeting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  addMeeting
+//
+//  Created by Madeline Webster on 4/3/18.
+//  Copyright © 2018 Madeline Webster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
